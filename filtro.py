c25460c7b2110c0bdbb3b290a6ae486f44a2eec9
lgm12345 <lucasgugelmaciel@gmail.com>
2024-01-22 10:38:35 -0300

    correcao

--- filtro.py
+++ filtro.py
@@ -8,8 +8,8 @@ query2 = "select rs.cd_rs,replace(replace(replace(substr(rs.ds_rs,1,900),chr(13)
 query3 = "select rs.cd_rs,replace(replace(replace(substr(rs.ds_rs,1,50),chr(13),''),chr(10),' '),'  ',' ') ds_rs,rs.dt_inicio,nvl(pe.nm_pessoa_resumido, pe.nm_fantasia) nm_empresa,nvl(pc.nm_pessoa_resumido, nvl(trim(substr(pc.nm_pessoa,1,instr(pc.nm_pessoa,' ',1))),'não informado')) nm_solicitante,nvl(pp.nm_pessoa_resumido, nvl(trim(substr(pp.nm_pessoa,1,instr(pp.nm_pessoa,' ',1))),'não informado')) nm_atendente,decode(rs.cd_tipo_prioridade, 231, 'BAIXA', 232, 'MÉDIA', 233, 'ALTA', 234, 'CRÍTICA') nm_prioridade from repoman.rs_temppp rs,repoman.pessoa pe,repoman.pessoa pc,repoman.pessoa pp,repoman.rs_fluxo_temppp f1 where rs.cd_pessoa_empresa = pe.cd_pessoa and rs.cd_pessoa_solicitante = pc.cd_pessoa and f1.cd_pessoa_responsavel = pp.cd_pessoa and rs.cd_contrato is not null and (rs.cd_pessoa_responsavel, rs.cd_pessoa_solicitante) not in (select 2,2 from dual) and f1.cd_fluxo = (select max(cd_fluxo) from repoman.rs_fluxo_temppp f2 where f2.cd_rs = rs.cd_rs and cd_tipo_status in (33,210)) and not exists (select 1 from repoman.rs_fluxo_temppp f3 where f3.cd_rs = rs.cd_rs and f3.cd_tipo_status in (34,230,45,211)) and rs.cd_rs = f1.cd_rs order by rs.cd_tipo_prioridade desc"
 def connecting_to_db():
     try:
-        dsn_tns = cx_Oracle.makedsn('192.168.33.11','1521', service_name='sgrprod')
-        conn = cx_Oracle.connect(user='bi', password='bi#2020#', dsn=dsn_tns)
+        dsn_tns = cx_Oracle.makedsn('XXXXXX','1521', service_name='sgrprod')
+        conn = cx_Oracle.connect(user='XX', password='YYYYY', dsn=dsn_tns)
     except cx_Oracle.DatabaseError:
         print('Banco de dados não encontrado, favor verifique sua conexão.')        
         sys.exit()          
